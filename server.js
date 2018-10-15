662a4cb90f19bbd2dddebeee141f9d8a6c66f7f7
Michael J. Lyons <mikelyons7@me.com>
2018-10-15 14:56:22 -0600

    Add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./routes/db');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/v1', db);
 app.listen(app.get('port'), () => {
   console.log(`${app.name} is running on port ${app.get('port')}.`);
